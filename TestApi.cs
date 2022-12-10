b7c4a0282a75a7b853d508a87f63db5c4fc0d1be
mohamad tolou <mb.tolou@gmail.com>
2022-12-10 20:57:03 +0330

    test cookie and cros origin

--- TestApi.cs
+++ TestApi.cs
@@ -14,4 +14,41 @@ class TestApi
 			}
 		return context.Request.Files.Count;
 	}
+
+}
+
+
+[Controller(BaseUrl = "api/cros-test/")]
+class CrosApi
+{
+	[Post]
+	[OptionsAttribute(AllowOrigin = "www.goooogle.com")]
+	public object getNowCustomOrigin(IHttpContext context)
+	{
+		return new { Time = DateTime.Now, Origin = context.Request.Header["Origin"] };
+	}
+
+	[Post]
+	public object getNow(IHttpContext context)
+	{
+		return new { Time = DateTime.Now, Origin = context.Request.Header["Origin"] };
+	}
+}
+
+
+[Controller(BaseUrl = "api/cookie-test/")]
+class CookieApi
+{
+	[Post]
+	public object successCookie(IHttpContext context)
+	{
+		HttpParse.AnalyzeCookie(context.Request.Header["Cookie"], context.Request.Cookies);
+		return new { Cookie = context.Request.Cookies["TestCookieToken"] };
+	}
+
+	[Post]
+	public object failCookie(IHttpContext context)
+	{
+		return new { Cookie = context.Request.Cookies["TestCookieToken"] };
+	}
 }
